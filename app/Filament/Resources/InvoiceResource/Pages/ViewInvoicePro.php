506c1d0328c0439704973ddc742d497b9de4e3ba
Erfan Sabouri <erfaansabouri@gmail.com>
2025-05-16 14:04:20 +0330

    fix

--- app/Filament/Resources/InvoiceResource/Pages/ViewInvoicePro.php
+++ app/Filament/Resources/InvoiceResource/Pages/ViewInvoicePro.php
@@ -7,6 +7,10 @@ use Filament\Actions;
 use Filament\Infolists\Infolist;
 use Filament\Resources\Pages\ViewRecord;
 use Filament\Infolists;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Components\Group;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\Grid;
 
 class ViewInvoicePro extends ViewRecord
 {
@@ -16,7 +20,31 @@ class ViewInvoicePro extends ViewRecord
     {
         return $infolist
             ->schema([
-                         Infolists\Components\TextEntry::make('id'),
+                         Section::make('اطلاعات پرداخت')
+                                ->schema([
+
+                                             TextEntry::make('tx_id')->label('کد تراکنش'),
+                                             TextEntry::make('payment_price')->label('مبلغ پرداختی'),
+                                             TextEntry::make('products_price')->label('مبلغ محصولات'),
+                                             TextEntry::make('shipping_price')->label('هزینه ارسال'),
+                                             TextEntry::make('discount')->label('تخفیف'),
+                                             TextEntry::make('coupon_code')->label('کد تخفیف'),
+                                         ])->columns(2),
+
+                         Section::make('مشخصات مشتری')
+                                ->schema([
+                                             TextEntry::make('first_name')->label('نام'),
+                                             TextEntry::make('last_name')->label('نام خانوادگی'),
+                                             TextEntry::make('phone')->label('شماره تماس'),
+                                             TextEntry::make('email')->label('ایمیل'),
+                                             TextEntry::make('postal_code')->label('کد پستی'),
+                                             TextEntry::make('address')->label('آدرس'),
+                                         ])->columns(2),
+
+                         Section::make('توضیحات')
+                                ->schema([
+                                             TextEntry::make('description')->label('توضیحات سفارش'),
+                                         ]),
                      ]);
     }
 }
