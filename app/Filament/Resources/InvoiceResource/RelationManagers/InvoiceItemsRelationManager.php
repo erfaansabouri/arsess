92472b42b19ad131f9cad0443e98b4c07d867210
Erfan Sabouri <erfaansabouri@gmail.com>
2025-05-16 12:22:55 +0330

    fix

--- app/Filament/Resources/InvoiceResource/RelationManagers/InvoiceItemsRelationManager.php
+++ app/Filament/Resources/InvoiceResource/RelationManagers/InvoiceItemsRelationManager.php
@@ -6,49 +6,43 @@ use Filament\Forms;
 use Filament\Tables;
 use Filament\Resources\RelationManagers\RelationManager;
 
-class InvoiceItemsRelationManager extends RelationManager
-{
+class InvoiceItemsRelationManager extends RelationManager {
     protected static string $relationship = 'invoiceItems';
-
     protected static ?string $recordTitleAttribute = 'id';
-
-    protected static ?string $label = 'محصولات خریداری شده';
+    protected static ?string $label       = 'محصولات خریداری شده';
     protected static ?string $pluralLabel = 'محصولات خریداری شده';
 
-    public static function canViewForRecord( $ownerRecord , string $pageClass ): bool
-    {
+    public static function canViewForRecord ( $ownerRecord , string $pageClass ): bool {
         return true;
     }
 
-    public static function canViewAny(): bool
-    {
+    public static function canViewAny (): bool {
         return true;
     }
 
-    public function canCreate(): bool
-    {
+    public function canCreate (): bool {
         return false;
     }
 
-    public function canEdit($record): bool
-    {
+    public function canEdit ( $record ): bool {
         return false;
     }
 
-    public function canDelete($record): bool
-    {
+    public function canDelete ( $record ): bool {
         return false;
     }
 
-    public function table(Tables\Table $table): Tables\Table
-    {
-        return $table
-            ->columns([
-                          Tables\Columns\TextColumn::make('product_id')->label('شناسه محصول'),
-                          Tables\Columns\TextColumn::make('quantity')->label('تعداد'),
-                          Tables\Columns\TextColumn::make('price')->label('قیمت'),
-                      ])
-            ->actions([]) // بدون اکشن
-            ->bulkActions([]); // بدون bulk
+    public function table ( Tables\Table $table ): Tables\Table {
+        return $table->columns([
+                                   // product.title
+                                      Tables\Columns\TextColumn::make('product.title')
+                                                                ->label('محصول') ,
+                                   Tables\Columns\TextColumn::make('quantity')
+                                                            ->label('تعداد') ,
+                                   Tables\Columns\TextColumn::make('price')
+                                                            ->label('قیمت') ,
+                               ])
+                     ->actions([]) // بدون اکشن
+                     ->bulkActions([]); // بدون bulk
     }
 }
--- app/Models/InvoiceItem.php
+++ app/Models/InvoiceItem.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class InvoiceItem extends Model
 {
-    //
+    public function invoice (): BelongsTo {
+        return $this->belongsTo(Invoice::class);
+    }
+
+    public function product (): BelongsTo {
+        return $this->belongsTo(Product::class);
+    }
 }
