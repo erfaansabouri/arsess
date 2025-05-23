47a72b3ee26ec9f1a27abb7a05bd1e76b2b18e8e
Erfan Sabouri <erfaansabouri@gmail.com>
2025-05-23 18:06:10 +0330

    fix

--- resources/views/arses/cart/show.blade.php
+++ resources/views/arses/cart/show.blade.php
@@ -22,7 +22,7 @@
                                     </li>
                                 </ul>
                                 <a href="{{ route('checkout.show') }}" class="setlmntBtn btn transitionCls">
-                                    <button>
+                                    <button class="setlmntBtn btn transitionCls">
                                         تسویه حساب
                                     </button>
                                 </a>
