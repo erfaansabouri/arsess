743dd16c7a2925ea8ef5aaba03d207e62cf65772
Erfan Sabouri <erfaansabouri@gmail.com>
2025-05-23 17:37:47 +0330

    fix

--- resources/views/arses/partials/top-moving-logo.blade.php
+++ resources/views/arses/partials/top-moving-logo.blade.php
@@ -1,63 +1,63 @@
 <section class="topLogoSec">
     <ul>
         <li>
-            <a href="#">
+            <a href="{{ route('home') }}">
                 <img src="{{ asset('arses/asset/img/topLogo01.svg') }}" alt="brand" />
             </a>
         </li>
         <li>
-            <a href="#">
+            <a href={{ route('home') }}>
                 <img src="{{ asset('arses/asset/img/topLogo02.svg') }}" alt="brand" />
             </a>
         </li>
         <li>
-            <a href="#">
+            <a href={{ route('home') }}>
                 <img src="{{ asset('arses/asset/img/topLogo01.svg') }}" alt="brand" />
             </a>
         </li>
         <li>
-            <a href="#">
+            <a href={{ route('home') }}>
                 <img src="{{ asset('arses/asset/img/topLogo02.svg') }}" alt="brand" />
             </a>
         </li>
         <li>
-            <a href="#">
+            <a href={{ route('home') }}>
                 <img src="{{ asset('arses/asset/img/topLogo01.svg') }}" alt="brand" />
             </a>
         </li>
         <li>
-            <a href="#">
+            <a href={{ route('home') }}>
                 <img src="{{ asset('arses/asset/img/topLogo02.svg') }}" alt="brand" />
             </a>
         </li>
 
         <li>
-            <a href="#">
+            <a href={{ route('home') }}>
                 <img src="{{ asset('arses/asset/img/topLogo01.svg') }}" alt="brand" />
             </a>
         </li>
         <li>
-            <a href="#">
+            <a href={{ route('home') }}>
                 <img src="{{ asset('arses/asset/img/topLogo02.svg') }}" alt="brand" />
             </a>
         </li>
         <li>
-            <a href="#">
+            <a href={{ route('home') }}>
                 <img src="{{ asset('arses/asset/img/topLogo01.svg') }}" alt="brand" />
             </a>
         </li>
         <li>
-            <a href="#">
+            <a href={{ route('home') }}>
                 <img src="{{ asset('arses/asset/img/topLogo02.svg') }}" alt="brand" />
             </a>
         </li>
         <li>
-            <a href="#">
+            <a href={{ route('home') }}>
                 <img src="{{ asset('arses/asset/img/topLogo01.svg') }}" alt="brand" />
             </a>
         </li>
         <li>
-            <a href="#">
+            <a href={{ route('home') }}>
                 <img src="{{ asset('arses/asset/img/topLogo02.svg') }}" alt="brand" />
             </a>
         </li>
